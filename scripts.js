85769e52f8ba8a6c86160e0fc9474706658ca8af
fuzzfactory0 <zekrom325@gmail.com>
2022-04-24 21:50:27 -0300

    Seat swap, UI & bug fixes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let main = document.getElementById('full');
-const apiURL = 'http://ec2-54-147-176-194.compute-1.amazonaws.com:3000';
-
+//const apiURL = 'http://ec2-54-147-176-194.compute-1.amazonaws.com:3000';
+const apiURL = 'http://localhost:3000';
 document.onkeydown = showBigCard;
 document.onkeyup = hideBigCard;
 
@@ -9,9 +9,7 @@ document.onkeyup = hideBigCard;
 //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO
 //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO
 //TODO                                                                                     TODO
-//TODO          code to play card, receive such data, show played cards on screen          TODO
-//TODO              code to keep played cards hidden until admin reveals them              TODO
-//TODO                   code to return a card to the deck and shuffle it                  TODO
+//TODO                          let the room admin arrange seats                           TODO
 //TODO              the game is playable as long as all of this is implemented             TODO
 //TODO                                                                                     TODO
 //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO //TODO TODO TODO TODO TODO TODO
@@ -24,27 +22,54 @@ let playerid = '';
 let sanmaSelected = false;
 let sessionState = '';
 let callInterval;
+let hiddenUI = false;
+let hiddenCards = false;
+let arrangeMode = false;
 
 let html = `
   <div id="saki-sidebar">
     <h2>Saki Cards</h2>
-    <h3 id="room-number"></h2>
-    <h3 id="room-admin"></h2>
+    <h3 id="room-number"></h3>
+    <h6 id="room-admin"></h6>
     <div id="game-controls">
-      <button id="hide-button" class="btn btn-secondary btn-sm w-100">Toggle UI</button>
+      <div class="mt-2 d-flex" style="width:100%;">
+        <button id="hide-button" class="btn btn-secondary btn-sm w-100" style="margin-right: 8px">Toggle UI</button>
+        <button id="hide-cards-button" class="btn btn-secondary btn-sm w-100" style="margin-right:0">Hide cards</button>
+      </div>
       <button id="reveal-button" class="btn btn-warning btn-sm w-100">Reveal cards</button>
       <button id="reset-button" class="btn btn-warning btn-sm w-100">New round</button>
       <button id="draw-button" class="btn btn-warning btn-sm w-100">Draw 1</button>
+      <button id="arrange-button" class="btn btn-dark btn-sm w-100">Arrange seats</button>
 
-      <p id="disclaimer">Very early alpha. This code is so jank and fragile it will break as soon as you do something too fancy. Please be patient
-        <p>Developed by Umeboshi (Discord: @Fuzz#7915)</p>
-        <p>Original game by Anton00, KlorofinMaster & DramaTheurgist</p>
-        <p>Saki 咲 Fan Community</p>
+      <div class="mt-2 d-flex" style="width:100%;">
+        <h3 class="seat-select" style="display: none; margin: 6px 6px 0 0;">🡄</h3>
+        <select id="select-kamicha" class="dark-select form-control form-control-sm mt-2 seat-select seat-select-element" style="display: none">
+        </select>
+      </div>
+
+      <div class="mt-2 d-flex" style="width:100%;">
+        <h3 class="seat-select" style="display: none; margin: 6px 6px 0 0;">🡅</h3>
+        <select id="select-toimen" class="dark-select form-control form-control-sm mt-2 seat-select seat-select-element" style="display: none">
+        </select>
+      </div>
+
+      <div class="mt-2 d-flex" style="width:100%;">
+        <h3 class="seat-select" style="display: none; margin: 6px 6px 0 0;">🡆</h3>
+        <select id="select-shimocha" class="dark-select form-control form-control-sm mt-2 seat-select seat-select-element" style="display: none">
+        </select>
+      </div>
+
+      <p id="disclaimer"><!--Very early alpha. This code is so jank and fragile it will break as soon as you do something too fancy. Please be patient-->
+        <p>Press shift while hovering over a card to zoom in.</p>
+        <p>Green button to play a card, red button to return it to the deck</p>
+        <p class="grey">Developed by Umeboshi (Discord: @Fuzz#7915)</p>
+        <p class="grey">Original game by Anton00, KlorofinMaster & DramaTheurgist</p>
+        <p class="grey">Saki 咲 Fan Community</p>
       </p>
     </div>
     <div id="join-controls"> 
       <h3>Create Room</h2>
-      <form id="create-form">
+      <form id="create-form" autocomplete="off">
         <select id="sanma-input" class="dark-select form-control form-control-sm mt-2">
           <option value="false">Suuma</option>
           <option value="true">Sanma</option>
@@ -55,7 +80,7 @@ let html = `
       </form>
 
       <h3>Join Room</h2>
-      <form id="join-form">
+      <form id="join-form" autocomplete="off">
         <label for="session-input">Room ID:</label>
         <input type="tel" id="session-input" class="form-control form-control-sm" minlength=4 maxlength=4 required pattern="^[0-9]{2,24}$"></input>
         <label for="nickname-input">Nickname:</label>
@@ -65,17 +90,17 @@ let html = `
     </div>
   </div>
   
-  <div id="saki-player-hand" style="bottom: -12vh;">
+  <div id="saki-player-hand" >
   </div>
 
   <div class="opponent-area">
-    <div id="kamicha-player-hand" class="opponent-hand" style="left: -16vw;">
+    <div id="kamicha-player-hand" class="opponent-hand" >
     </div>
 
-    <div id="toimen-player-hand" class="opponent-hand" style="top: -12vh;">
+    <div id="toimen-player-hand" class="opponent-hand">
     </div>
 
-    <div id="shimocha-player-hand" class="opponent-hand" style="right: -16vw;">
+    <div id="shimocha-player-hand" class="opponent-hand">
     </div>
   </div>
 
@@ -83,16 +108,20 @@ let html = `
     <img id="big-saki-card-img" class="saki-card-img" src="" alt="" draggable="false">
   </div>
 
-  <div id="player-card" class="played-card" style="bottom: 10vh">
+  <h3 class="player-name" id="kamicha-name" style="top: 48vh; left: 29vw;"></h3>
+  <h3 class="player-name" id="toimen-name" style="top: 22vh; left: 48vw;"></h3>
+  <h3 class="player-name" id="shimocha-name" style="top: 48vh; right: 29vw;"></h3>
+
+  <div id="player-card" class="played-card">
     <img id="player-img" class="played-saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="" draggable="false">
   </div>
-  <div id="kamicha-card" class="played-card" style="left: 14vw">
+  <div id="kamicha-card" class="played-card">
     <img id="kamicha-img" class="played-saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="" draggable="false">
   </div>
-  <div id="toimen-card" class="played-card" style="top: 2vh">
+  <div id="toimen-card" class="played-card">
     <img id="toimen-img" class="played-saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="" draggable="false">
   </div>
-  <div id="shimocha-card" class="played-card" style="right: 14vw">
+  <div id="shimocha-card" class="played-card">
     <img id="shimocha-img" class="played-saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="" draggable="false">
   </div>
 `;
@@ -104,6 +133,10 @@ const kamichaHand = document.getElementById('kamicha-player-hand');
 const toimenHand = document.getElementById('toimen-player-hand');
 const shimochaHand = document.getElementById('shimocha-player-hand');
 const bigCard = document.getElementById('big-card');
+const pcard = document.getElementById('player-card');
+const kcard = document.getElementById('kamicha-card');
+const tcard = document.getElementById('toimen-card');
+const scard = document.getElementById('shimocha-card');
 
 for (let el of document.getElementsByName('input')) {
   el.addEventListener("keypress", function(event) {
@@ -120,14 +153,113 @@ addCardListeners(document.getElementById('shimocha-card'));
 
 
 document.getElementById("hide-button").addEventListener("click", () => {
-  playerHand.style.bottom = playerHand.style.bottom == '-12vh' ? '-500px' : '-12vh';
-  kamichaHand.style.left = kamichaHand.style.left == '-16vw' ? '-500px' : '-16vw';
-  toimenHand.style.top = toimenHand.style.top == '-12vh' ? '-500px' : '-12vh';
-  shimochaHand.style.right = shimochaHand.style.right == '-16vw' ? '-500px' : '-16vw';
-  document.getElementById('player-card').style.bottom = document.getElementById('player-card').style.bottom == '10vh' ? '-500px' : '10vh'
-  document.getElementById('kamicha-card').style.left = document.getElementById('kamicha-card').style.left == '14vw' ? '-500px' : '14vw';
-  document.getElementById('toimen-card').style.top = document.getElementById('toimen-card').style.top == '2vh' ? '-500px' : '2vh';
-  document.getElementById('shimocha-card').style.right = document.getElementById('shimocha-card').style.right == '14vw' ? '-500px' : '14vw';
+  hiddenUI = !hiddenUI;
+
+  if (hiddenUI) {
+    playerHand.style.bottom = '-500px';
+    kamichaHand.style.left = '-500px';
+    toimenHand.style.top = '-500px';
+    shimochaHand.style.right = '-500px';
+  
+    pcard.style.bottom = '1vh';
+    pcard.style.right = '0vw';
+    pcard.style.height = '31vh';
+    
+    kcard.style.top = '61vh';
+    kcard.style.left = '32vw';
+    kcard.style.height = '20vh';
+    kcard.style.transform = 'rotate(90deg)';
+
+    tcard.style.top = '20vh';
+    tcard.style.left = '31.5vw';
+    tcard.style.height = '20vh';
+    tcard.style.transform = 'rotate(180deg)';
+
+    scard.style.top = '18.5vh';
+    scard.style.right = '32vw';
+    scard.style.height = '20vh';
+    scard.style.transform = 'rotate(-90deg)';
+  } else {
+    playerHand.style.bottom = null;
+    kamichaHand.style.left = null;
+    toimenHand.style.top = null;
+    shimochaHand.style.right = null;
+  
+    pcard.style.bottom = null;
+    pcard.style.right = null;
+    pcard.style.height = null;
+    
+    kcard.style.top = null;
+    kcard.style.left = null;
+    kcard.style.height = null;
+    kcard.style.transform = null;
+
+    tcard.style.top = null;
+    tcard.style.left = null;
+    tcard.style.height = null;
+    tcard.style.transform = null;
+
+    scard.style.top = null;
+    scard.style.right = null;
+    scard.style.height = null;
+    scard.style.transform = null;
+  }
+  
+});
+
+document.getElementById("hide-cards-button").addEventListener("click", () => {
+  hiddenCards = !hiddenCards;
+
+  if (hiddenCards) {  
+    document.getElementById('player-img').style.display = 'none';
+    document.getElementById('kamicha-img').style.display = 'none';
+    document.getElementById('toimen-img').style.display = 'none';
+    document.getElementById('shimocha-img').style.display = 'none';
+  } else {
+    document.getElementById('player-img').style.display = 'block';
+    document.getElementById('kamicha-img').style.display = 'block';
+    document.getElementById('toimen-img').style.display = 'block';
+    document.getElementById('shimocha-img').style.display = 'block';
+  }
+  
+});
+
+document.getElementById("arrange-button").addEventListener("click", () => {
+  if (sessionState.players.length > 2) {
+    if (arrangeMode) {
+      arrangeMode = false;
+      for (let element of document.getElementsByClassName('seat-select')) {
+        element.style.display = 'none';
+      }
+      let button = document.getElementById("arrange-button");
+      button.textContent = 'Arrange seats';
+      button.className = 'btn btn-dark btn-sm w-100';
+    } else {
+      arrangeMode = true;
+      
+      for (let element of document.getElementsByClassName('seat-select')) {
+        element.style.display = 'block';
+      }
+      let button = document.getElementById("arrange-button");
+      button.textContent = 'Confirm';
+      button.className = 'btn btn-danger btn-sm w-100';
+    }
+  }
+});
+
+document.getElementById("select-kamicha").addEventListener("change", () => {
+  let value = document.getElementById("select-kamicha").value;
+  if (value != null) arrangeSeats(4, value);
+});
+
+document.getElementById("select-toimen").addEventListener("change", () => {
+  let value = document.getElementById("select-toimen").value;
+  if (value != null) arrangeSeats(3, value);
+});
+
+document.getElementById("select-shimocha").addEventListener("change", () => {
+  let value = document.getElementById("select-shimocha").value;
+  if (value != null) arrangeSeats(2, value);
 });
 
 document.getElementById("create-button").addEventListener("click", () => {  
@@ -217,6 +349,9 @@ function addCardListeners(card) {
 }
 
 function receiveData(session) {
+  console.log(document.getElementById('select-kamicha').value);
+  console.log(document.getElementById('select-toimen').value);
+  console.log(document.getElementById('select-shimocha').value);
   if (JSON.stringify(session) != JSON.stringify(sessionState)) {
     console.log(session);
     sessionid = session.id;
@@ -278,6 +413,34 @@ function receiveData(session) {
       break;
     }
 
+    if (JSON.stringify(session.players) != JSON.stringify(sessionState.players) && session.owner == player.nickname) {
+      for (let element of document.getElementsByClassName('seat-select-element')) {
+        element.innerHTML = '<option value> None </option>';
+      }
+      session.players.forEach(p => {
+        if (p.nickname != player.nickname) {
+          let option = `<option value="${p.nickname}">${p.nickname}</option>`;
+            
+          document.getElementById('select-kamicha').innerHTML += option;
+          document.getElementById('select-toimen').innerHTML += option;
+          document.getElementById('select-shimocha').innerHTML += option;
+        }
+      });
+      session.players.forEach(p => {
+        switch(p.seat) {
+          case 4: 
+            document.getElementById('select-kamicha').value = p.nickname;
+          break;
+          case 3:
+            document.getElementById('select-toimen').value = p.nickname;
+          break;
+          case 2:
+            document.getElementById('select-shimocha').value = p.nickname;
+          break;
+        }
+      });
+    }
+
     kamichaHand.textContent = '';
     toimenHand.textContent = '';
     shimochaHand.textContent = '';
@@ -285,6 +448,7 @@ function receiveData(session) {
 
     if (session.players.some(p => p.seat == kamicha)) {
       let thisPlayer = session.players.find(p => p.seat == kamicha);
+      document.getElementById('kamicha-name').textContent = thisPlayer.nickname;
       if (thisPlayer.playedCard != null) {
         document.getElementById('kamicha-card').style.display = 'block';
         if (session.revealed) {
@@ -304,10 +468,15 @@ function receiveData(session) {
         newcard.innerHTML = `<img class="saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="Unknown card" draggable="false">`;
         kamichaHand.insertAdjacentElement('beforeend', newcard);
       }
+    } 
+    else {
+      document.getElementById('kamicha-name').textContent = '';
+      document.getElementById('kamicha-card').style.display = 'none';
     }
 
     if (session.players.some(p => p.seat == toimen)) {
       let thisPlayer = session.players.find(p => p.seat == toimen);
+      document.getElementById('toimen-name').textContent = thisPlayer.nickname;
       if (thisPlayer.playedCard != null) {
         document.getElementById('toimen-card').style.display = 'block';
         if (session.revealed) {
@@ -327,10 +496,15 @@ function receiveData(session) {
         newcard.innerHTML = `<img class="saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="Unknown card" draggable="false">`;
         toimenHand.insertAdjacentElement('beforeend', newcard);
       }
+    } 
+    else {
+      document.getElementById('toimen-name').textContent = '';
+      document.getElementById('toimen-card').style.display = 'none';
     }
 
     if (session.players.some(p => p.seat == shimocha)) {
       let thisPlayer = session.players.find(p => p.seat == shimocha);
+      document.getElementById('shimocha-name').textContent = thisPlayer.nickname;
       if (thisPlayer.playedCard != null) {
         document.getElementById('shimocha-card').style.display = 'block';
         if (session.revealed) {
@@ -350,15 +524,16 @@ function receiveData(session) {
         newcard.innerHTML = `<img class="saki-card-img" src="${chrome.runtime.getURL('assets/cardback.png')}" alt="Unknown card" draggable="false">`;
         shimochaHand.insertAdjacentElement('beforeend', newcard);
       }
+    } 
+    else {
+      document.getElementById('shimocha-name').textContent = '';
+      document.getElementById('shimocha-card').style.display = 'none';
     }
-    
-
 
     sessionState = session;
   }
 }
 
-//let messages;
 init = function() {
   callInterval = setInterval(() => {
     fetch(`${apiURL}/session?sessionid=${sessionid}&playerid=${playerid}`, {
@@ -467,6 +642,8 @@ joinRoom = () => {
     document.getElementById('room-admin').textContent = 'Admin: ' + data.owner;
     document.getElementById("reveal-button").style.display = 'none';
     document.getElementById("reset-button").style.display = 'none';
+    document.getElementById("arrange-button").style.display = 'none';
+    
     
     receiveData(data);
     init();
@@ -599,6 +776,32 @@ revealTable = () => {
 
 resetTable = () => {
   fetch(`${apiURL}/reset?sessionid=${sessionid}&playerid=${playerid}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(async response => {
+    const isJson = response.headers.get('content-type')?.includes('application/json');
+    const data = isJson ? await response.json() : null;
+
+    // check for error response
+    if (!response.ok) {
+      // get error message from body or default to response status
+      const error = (data && data.message) || response.status;
+      return Promise.reject(error);
+    }
+    
+    receiveData(data);
+  })
+  .catch(error => {
+    //todo element.parentElement.innerHTML = `Error: ${error}`;
+    console.error('There was an error!', error);
+  });
+}
+
+arrangeSeats = (seat, player) => {
+  fetch(`${apiURL}/arrange?seat=${seat}&target=${player}&playerid=${playerid}&sessionid=${sessionid}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
